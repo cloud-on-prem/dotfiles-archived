61ff5d631999fff8f1468411fdd3b3f4557d5cba
❅ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2014-08-30 17:49:59 +1000

    Add docker and other required deps.

--- babushka-deps/osx.rb
+++ babushka-deps/osx.rb
@@ -1,5 +1,42 @@
 dep 'osx' do
-  requires 'docker.bin'
+  requires 'homebrew.pkg'
+  requires 'virtualbox'
+  requires 'brew cask'
+  requires 'boot2docker.bin'
+  requires 'vagrant'
 end
 
-dep 'docker.bin'
+dep 'homebrew.pkg'
+dep 'virtualbox.bin'
+
+dep 'brew cask' do
+  met? do
+    shell? "brew list brew-cask"
+  end
+
+  meet do
+    shell 'brew tap phinze/homebrew-cask && brew install brew-cask'
+  end
+end
+
+dep 'boot2docker.bin'
+
+dep 'virtualbox' do
+  met? do
+    shell? "virtualbox -v"
+  end
+
+  meet do
+    shell "brew cask install virtualbox"
+  end
+end
+
+dep 'vagrant' do
+  met? do
+    shell? "vagrant -v"
+  end
+
+  meet do
+    shell "brew cask install vagrant"
+  end
+end
