d443998bb44e30305e90457edd402e387d9abcdf
❅ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2014-08-31 14:42:00 +1000

    Add a dev dep.

---
+++ babushka-deps/dev.rb
@@ -0,0 +1,20 @@
+dep 'dev' do
+  requires 'vim'
+end
+
+dep 'vim' do
+  requires 'vundle'
+end
+
+dep 'vundle' do
+  file = "~/.vim/bundle/Vundle.vim"
+
+  met? do
+    file.p.exists?
+  end
+
+  meet do
+    shell "git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim"
+    shell "vim +PluginInstall +qall"
+  end
+end
