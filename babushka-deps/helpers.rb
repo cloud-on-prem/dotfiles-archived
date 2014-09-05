08250299c36cd83425bcbd78153c538499434a07
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2014-09-06 00:04:19 +1000

    Add a helper class for babushka common tasks.

---
+++ babushka-deps/helpers.rb
@@ -0,0 +1,20 @@
+module Babushka
+  module Helpers
+    class Os
+      class << self
+        def osx?
+          `uname` == "Darwin"
+        end
+
+        def linux?
+          !osx?
+        end
+
+        def windows?
+          raise "Seriously?"
+        end
+      end
+    end
+  end
+end
+
