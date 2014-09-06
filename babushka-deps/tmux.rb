e3d2f812125b1c6184ee3a5aec0d940b196843ed
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2014-09-07 00:28:23 +1000

    Add battery status to Tmux.

--- babushka-deps/tmux.rb
+++ babushka-deps/tmux.rb
@@ -2,6 +2,7 @@ dep 'tmux-main' do
   requires 'tmux.lib'
   requires 'tmuxinator.gem'
   requires 'tmuxinator-templates'
+  requires 'battery-status'
 end
 
 dep 'tmux.lib'
@@ -24,5 +25,16 @@ dep 'tmuxinator-templates' do
 
   meet do
     shell "ln -sfn #{real_file} #{sym_file}"
+  end
+end
+
+dep 'battery-status' do
+  met? do
+    shell?("/usr/bin/battery")
+  end
+
+  meet do
+    `sudo cp ~/.dotfiles/scripts/battery.sh /usr/bin/battery`
+    `sudo chmod +x /usr/bin/battery`
   end
 end
