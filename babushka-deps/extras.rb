ee7fe632afe39ca8b1dbeee2ddc1d85f406b117d
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-05-08 11:58:30 +0000

    Fix postgres dep.

--- babushka-deps/extras.rb
+++ babushka-deps/extras.rb
@@ -1,7 +1,13 @@
 dep 'postgresql.bin' do
+  unless Babushka::Helpers::Os.osx?
+    requires 'libpq-dev.managed'
+  end
+
   met? do
-    shell? "postgres --version"
+    shell? "psql --version"
   end
 end
+
+dep 'libpq-dev.managed' do provides [] end
 
 dep 'phantomjs.managed'   do provides [] end
